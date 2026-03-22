6eac12ea6b80c6367bc32d5409835ceb47fbe79b
William Tremblay <tremwil@gmail.com>
2026-03-22 16:40:06 +0000

    chore: add ctor fn signature

--- tests/ctor.rs
+++ tests/ctor.rs
@@ -1,18 +1,28 @@
-pub mod outer {
-    use moveit2_proc_macros::Ctor;
+use moveit2::{New, ctor, new};
+use pin_project::pin_project;
 
-    #[derive(Ctor)]
-    pub struct Test<T>
-    where
-        T: Clone,
-    {
-        /// Foo doc.
-        #[cfg(abc)]
-        pub foo: usize,
-        /// Bar doc.
-        pub(super) bar: T,
-        /// Baz doc 1
-        /// Baz doc 2
-        pub(crate) baz: String,
+#[ctor]
+#[pin_project]
+pub struct SelfReferential<T> {
+    #[pin]
+    val: T,
+    addr_of_val: *mut T,
+}
+
+impl<T> SelfReferential<T> {
+    pub fn construct(val_ctor: impl New<Output = T>) -> impl New<Output = Self> {
+        Self::ctor(|b| {
+            let addr = b.val.as_ptr().cast_mut();
+            b.val(val_ctor).addr_of_val(new::of(addr))
+        })
     }
 }
+
+// impl<T> SelfReferential<T> {
+//     pub fn construct(val_ctor: impl New<Output = T>) -> impl New<Output = Self> {
+//         Self::ctor(|mut fields| Builder::<Self> {
+//             addr_of_val: ctor::emplace(val_ctor),
+//             val: ctor::write(fields.val.as_ptr()),
+//         })
+//     }
+// }
